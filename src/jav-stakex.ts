dd79f524def14dcb2f53c34dc039ffee0981ceee
meiswinkel1991 <florian.meiswinkel@outlook.de>
2024-07-03 10:25:43 +0200

    add staking info entity

--- src/jav-stakex.ts
+++ src/jav-stakex.ts
@@ -1,51 +1,103 @@
+import { BigDecimal, BigInt } from "@graphprotocol/graph-ts";
 import {
-    Claim,
-    Stake, Unstake
-} from "../generated/JavStakeX/JavStakeX"
-import {
-    StakeLog,
-} from "../generated/schema"
-import {weiToEth} from "./util";
+  Claim,
+  JavStakeX,
+  Stake,
+  Unstake,
+} from "../generated/JavStakeX/JavStakeX";
+import { JavTokenBalance, StakeInfo, StakeLog } from "../generated/schema";
+import { getPoolId, weiToEth } from "./util";
 
 export function handleClaim(event: Claim): void {
-    let entity = new StakeLog(event.transaction.hash.concatI32(event.logIndex.toI32()))
+  let entity = new StakeLog(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
 
-    entity.amount = event.params._amount
-    entity.amountEth = weiToEth(event.params._amount)
-    entity.address = event.params._user
-    entity.tx = event.transaction.hash
-    entity.block = event.block.number
-    entity.date = event.block.timestamp
+  entity.amount = event.params._amount;
+  entity.amountEth = weiToEth(event.params._amount);
+  entity.address = event.params._user;
+  entity.tx = event.transaction.hash;
+  entity.block = event.block.number;
+  entity.date = event.block.timestamp;
 
-    entity.type = "CLAIM"
-    entity.save()
+  entity.type = "CLAIM";
+  entity.save();
+
+  let pid = getPoolId(event.params._token);
+  let stakeInfo = StakeInfo.load(event.params._user.concatI32(pid.toI32()));
+
+  if (stakeInfo) {
+    stakeInfo.claimedAmount = stakeInfo.claimedAmount.plus(
+      event.params._amount
+    );
+    stakeInfo.claimedAmountEth = weiToEth(stakeInfo.claimedAmount);
+    stakeInfo.save();
+  }
 }
 
 export function handleStake(event: Stake): void {
-    let entity = new StakeLog(event.transaction.hash.concatI32(event.logIndex.toI32()))
+  let entity = new StakeLog(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
 
-    entity.amount = event.params._amount
-    entity.amountEth = weiToEth(event.params._amount)
-    entity.address = event.params._address
-    entity.tx = event.transaction.hash
-    entity.block = event.block.number
-    entity.date = event.block.timestamp
+  entity.amount = event.params._amount;
+  entity.amountEth = weiToEth(event.params._amount);
+  entity.address = event.params._address;
+  entity.tx = event.transaction.hash;
+  entity.block = event.block.number;
+  entity.date = event.block.timestamp;
 
-    entity.type = "STAKE"
-    entity.save()
+  entity.type = "STAKE";
+  entity.save();
+
+  let stakeInfo = StakeInfo.load(
+    event.params._address.concatI32(event.params._pid.toI32())
+  );
+
+  if (stakeInfo == null) {
+    stakeInfo = new StakeInfo(
+      event.params._address.concatI32(event.params._pid.toI32())
+    );
+    stakeInfo.pool = event.params._pid;
+    stakeInfo.address = event.params._address;
+    stakeInfo.claimedAmount = BigInt.zero();
+    stakeInfo.claimedAmountEth = BigDecimal.zero();
+    stakeInfo.totalStake = BigInt.zero();
+    stakeInfo.totalStakeEth = BigDecimal.zero();
+
+    let tokenBalance = JavTokenBalance.load(event.params._address);
+    if (tokenBalance) {
+      stakeInfo.freeTokenBalance = tokenBalance.id;
+    }
+  }
+
+  stakeInfo.totalStake = stakeInfo.totalStake.plus(event.params._amount);
+  stakeInfo.totalStakeEth = weiToEth(stakeInfo.totalStake);
+
+  stakeInfo.save();
 }
 
-
 export function handleUnstake(event: Unstake): void {
-    let entity = new StakeLog(event.transaction.hash.concatI32(event.logIndex.toI32()))
+  let entity = new StakeLog(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
 
-    entity.amount = event.params._amount
-    entity.amountEth = weiToEth(event.params._amount)
-    entity.address = event.params._address
-    entity.tx = event.transaction.hash
-    entity.block = event.block.number
-    entity.date = event.block.timestamp
+  entity.amount = event.params._amount;
+  entity.amountEth = weiToEth(event.params._amount);
+  entity.address = event.params._address;
+  entity.tx = event.transaction.hash;
+  entity.block = event.block.number;
+  entity.date = event.block.timestamp;
 
-    entity.type = "UNSTAKE"
-    entity.save()
+  entity.type = "UNSTAKE";
+  entity.save();
+
+  let stakeInfo = StakeInfo.load(
+    event.params._address.concatI32(event.params._pid.toI32())
+  );
+  if (stakeInfo) {
+    stakeInfo.totalStake = stakeInfo.totalStake.minus(event.params._amount);
+    stakeInfo.totalStakeEth = weiToEth(stakeInfo.totalStake);
+    stakeInfo.save();
+  }
 }
--- src/jav-token.ts
+++ src/jav-token.ts
@@ -1,21 +1,41 @@
-import {
-    Transfer,
-} from "../generated/JavToken/JavToken"
-import {
-    JavTokenLog,
-} from "../generated/schema"
-import {weiToEth} from "./util";
+import { BigDecimal, BigInt } from "@graphprotocol/graph-ts";
+import { Transfer } from "../generated/JavToken/JavToken";
+import { JavTokenBalance, JavTokenLog } from "../generated/schema";
+import { weiToEth } from "./util";
 
 export function handleTransfer(event: Transfer): void {
-    let entity = new JavTokenLog(event.transaction.hash.concatI32(event.logIndex.toI32()))
+  let entity = new JavTokenLog(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
 
-    entity.amount = event.params.value
-    entity.amountEth = weiToEth(event.params.value)
-    entity.from = event.params.from
-    entity.to = event.params.to
-    entity.tx = event.transaction.hash
-    entity.block = event.block.number
-    entity.date = event.block.timestamp
-    entity.save()
+  entity.amount = event.params.value;
+  entity.amountEth = weiToEth(event.params.value);
+  entity.from = event.params.from;
+  entity.to = event.params.to;
+  entity.tx = event.transaction.hash;
+  entity.block = event.block.number;
+  entity.date = event.block.timestamp;
+  entity.save();
+
+  let balanceFrom = JavTokenBalance.load(event.params.from);
+  if (balanceFrom == null) {
+    balanceFrom = new JavTokenBalance(event.params.from);
+    balanceFrom.balance = BigInt.zero();
+    balanceFrom.balanceEth = BigDecimal.zero();
+  }
+
+  balanceFrom.balance = balanceFrom.balance.minus(event.params.value);
+  balanceFrom.balanceEth = weiToEth(balanceFrom.balance);
+  balanceFrom.save();
+
+  let balanceTo = JavTokenBalance.load(event.params.to);
+  if (balanceTo == null) {
+    balanceTo = new JavTokenBalance(event.params.to);
+    balanceTo.balance = BigInt.zero();
+    balanceTo.balanceEth = BigDecimal.zero();
+  }
+
+  balanceTo.balance = balanceTo.balance.plus(event.params.value);
+  balanceTo.balanceEth = weiToEth(balanceTo.balance);
+  balanceTo.save();
 }
-
