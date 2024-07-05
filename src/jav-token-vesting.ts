113c3e7316824a60f497b816af14f0fe87ec3233
Igor Shelkovenkov <is@topiet.de>
2024-07-05 14:59:32 +0200

    add jav vesting and vesting release, 1.0.0 completed

---
+++ src/jav-token-vesting.ts
@@ -0,0 +1,45 @@
+import { weiToEth} from "./util";
+import {JavVestingLog, JavVestingReleaseLog} from "../generated/schema";
+import {Released, VestingScheduleAdded} from "../generated/TokenVesting/TokenVesting";
+import {BigInt} from "@graphprotocol/graph-ts";
+
+export function handleRelease(event: Released): void {
+  let entity = new JavVestingReleaseLog(
+      event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+
+  entity.amount = event.params.amount;
+  entity.amountEth = weiToEth(event.params.amount);
+  entity.address = event.params.to;
+  entity.vestingScheduleId = event.params.vestingScheduleId
+
+  entity.tx = event.transaction.hash;
+  entity.block = event.block.number;
+  entity.date = event.block.timestamp;
+
+  entity.save();
+}
+
+export function handleVesting(event: VestingScheduleAdded): void {
+  let entity = new JavVestingLog(
+      event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+
+  entity.amount = event.params.amountTotal;
+  entity.amountEth = weiToEth(event.params.amountTotal);
+
+  entity.address = event.params.beneficiary;
+  entity.cliff  = event.params.cliff;
+  entity.start  = event.params.start;
+  entity.duration  = event.params.duration;
+  entity.slicePeriodSeconds  = event.params.slicePeriodSeconds;
+  entity.revocable  = event.params.revocable;
+  entity.vestingType = BigInt.fromI32(event.params.vestingType);
+
+  entity.tx = event.transaction.hash;
+  entity.block = event.block.number;
+  entity.date = event.block.timestamp;
+
+
+  entity.save();
+}
